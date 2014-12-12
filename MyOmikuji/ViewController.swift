cbdd6a083468b9c868e284303ce63e47ebd712be
Hiroyuki Tsuruda <mocchicc@me.com>
2014-12-12 21:43:00 +0900

    using arc4random_uniform(UInt31)

--- MyOmikuji/ViewController.swift
+++ MyOmikuji/ViewController.swift
@@ -15,15 +15,23 @@ class ViewController: UIViewController {
     
     @IBAction func getOmikuji(sender: AnyObject) {
         // 占うよ! ボタンを押したら実行される関数
+        
         // 配列におみくじの結果を用意
         let results = [
                 "大吉",
                 "吉",
                 "中吉",
+                "末吉",
                 "凶"
         ]
-        //乱数を生成
         
+        // 乱数を生成
+        // 0 - n
+        // arc4random_uniform(n + 1)
+        
+        let random = arc4random_uniform(UInt32(results.count))
+        
+        // result[乱数] を resultLabel に表示
         
     }
     
